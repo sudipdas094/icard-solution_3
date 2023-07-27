4b6ee70c9f23531b560821b9fff7f2f805b1315c
Sudip Ranjan Das <sudipdas.094@gmail.com>
2023-07-27 21:57:23 +0530

    test#:wq

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AdminLteStudentForm from './AdminLTELayout_student.js';
+import AdminLteSchoolForm from './AdminLTELayout_school.js';
+import AdminLteSchoolSuccessForm from './AdminLTELayout_school_success.js'
 
-function App() {
+import SchoolDetails from './SchoolDetails.js';
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Routes>
+        <Route exact path="/school" element={<AdminLteSchoolForm />}  />
+        <Route path="/student" element={<AdminLteStudentForm />} />
+        <Route path="school/schoolSuccess" element={(<AdminLteSchoolSuccessForm />)}/>
+      </Routes>
+    </Router>
+    
   );
 }
 
